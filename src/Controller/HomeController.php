4f609fc1901fe131b87d02de5d4daadb727f466c
Gerald Col <gnut@gnut.eu>
2021-11-15 10:31:14 +0100

    Setting up the pagination of the homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +13,27 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(TrickRepository $trickRepository): Response
+    public function index(TrickRepository $trickRepository, Request $request): Response
     {
+        // $page = (int)$request->query->get("page", 1); provoque une erreur si ?page=toto ou 0
+        if(!is_numeric($request->query->get("page", 1)) or (int)$request->query->get("page", 1) <= 0) {
+            $page = 1 ;
+        }  else {
+            $page = (int)$request->query->get("page", 1);
+        }
+
+        $limit = 6 ;//on veut  enregistrement par pages 6
+
+        $start = $limit * $page - $limit; //calcul de l'offset(le début)
+        $total = count($trickRepository->findAll());//Calcul le nombre d'enregistrement
+
+        $pages = ceil($total/$limit);// nbrs de page total arondi à l'entier supérieur
+        $tricks=$trickRepository->findBy([],[],$limit,$start);
+
         return $this->render('home/index.html.twig', [
-            'tricks' => $trickRepository->findAll()
+            'tricks' => $tricks,
+            'page'=>$page,
+            'pages'=>$pages
         ]);
     }
     
