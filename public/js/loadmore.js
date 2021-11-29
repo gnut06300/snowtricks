818fe89b6e7f7e254af24d2899877f914c4b4849
Gerald Col <gnut@gnut.eu>
2021-11-29 23:55:22 +0100

    load more button

---
+++ public/js/loadmore.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  loadmoreButton = document.getElementById("loadmore");
+  updateLink(loadmoreButton);
+  console.log(loadmoreButton);
+  
+  // When click on "Load more" button, load next page items in ajax just before the button
+  loadmoreButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const el = this;
+      axios.get(el.getAttribute('href')).then(function (response) {
+      el.parentElement.insertAdjacentHTML('beforebegin', response.data);
+      updateLink(el);
+    });
+  });
+
+  // Check the html content of the previous element and update or remove load more button
+  function updateLink(el) {
+    let parent = el.parentElement;
+    console.log(parent)
+    let link = parent.previousElementSibling.innerHTML;
+    if (link) {
+      el.setAttribute('href', link);
+    } else {
+      parent.remove();
+    }
+  }
+    //let nextTricks = document.getElementById("nextTricks");
+    //console.log(nextTricks);
+    //let link = nextTricks.innerHTML;
+    // if (link) {
+    //   el.setAttribute('href', link);
+    // } else {
+      //nextTricks.remove();
+    
+});
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/home2", name="homepage2")
      */
     public function index(TrickRepository $trickRepository, Request $request): Response
     {
@@ -22,7 +22,7 @@ class HomeController extends AbstractController
             $page = (int)$request->query->get("page", 1);
         }
 
-        $limit = 10 ;//we want 6 records per page
+        $limit = 10 ;//we want 10 records per page
 
         $start = $limit * $page - $limit; //offset calculation (the start)
         $total = count($trickRepository->findAll());//Calculate the number of records
@@ -37,5 +37,52 @@ class HomeController extends AbstractController
             'arrow'=>true
         ]);
     }
-    
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home(): Response
+    {
+       return $this->render('home/index_1.html.twig', [
+            'arrow' => true
+        ]);
+    }
+
+    /**
+     * @Route("/tricks", name="tricks_list")
+     * @Route("/tricks/{page<\d+>}/{limit}-per-page", name="trick_page_with_limit", methods={"GET"})
+     */
+    public function tiricksList(TrickRepository $trickRepository, $page = 1, int $limit = 10): Response
+    {
+        // $page = (int)$request->query->get("page", 1); provoque une erreur si ?page=toto ou 0
+        // if (!is_numeric($request->query->get("page", 1)) or (int)$request->query->get("page", 1) <= 0) {
+        //     $page = 1;
+        // } else {
+        //     $page = (int)$request->query->get("page", 1);
+        // }
+
+        // $limit = (int)$request->query->get("limit", 10); //we want 10 records per page
+
+        $start = $limit * $page - $limit; //offset calculation (the start)
+        $total = count($trickRepository->findAll()); //Calculate the number of records
+
+        $pages = ceil($total / $limit); // total page count rounded to the next whole number
+        $tricks = $trickRepository->findBy([], ['createdAt' => 'DESC'], $limit, $start);
+
+        return $this->render('home/_tricksList.html.twig', [
+            'tricks' => $tricks,
+            'page' => $page,
+            'pages' => $pages,
+            'limit' => $limit,
+        ]);
+    }
+
+    /**
+     * @Route("/onload", name="onload")
+     */
+    public function onLoad(Request $request)
+    {
+        $newLimit = (int)$request->query->get("limit",10);
+        return $this->json(['code' => 200, 'message' => 'Ca marche bien', 'limit' => $newLimit], 200);
+    }
 }
