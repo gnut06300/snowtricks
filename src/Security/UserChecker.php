97fe6c6a95c4bf412b7a9004ab0fdd117d83f875
Gerald Col <gnut@gnut.eu>
2021-12-02 19:33:37 +0100

    Error 403 User Checker new Email

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -3,13 +3,22 @@
 namespace App\Security;
 
 use App\Entity\User as AppUser;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccountExpiredException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
-use Symfony\Component\Security\Core\User\UserCheckerInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserChecker implements UserCheckerInterface
 {
+    private $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
+
     public function checkPreAuth(UserInterface $user): void
     {
         if (!$user instanceof AppUser) {
@@ -17,8 +26,9 @@ class UserChecker implements UserCheckerInterface
         }
 
         if (!$user->isVerified()) {
+            $this->resendVerifEmail($user);
             // the message passed to this exception is meant to be displayed to the user
-            throw new CustomUserMessageAccountStatusException('Votre compte n\'a pas été vérifié.');
+            throw new CustomUserMessageAccountStatusException('Votre compte n\'a pas été vérifié. Un nouveau email de confirmation vous à été envoyé.');
         }
     }
 
@@ -32,5 +42,18 @@ class UserChecker implements UserCheckerInterface
         // if (!$user->isVerified()) {
         //     throw new AccountExpiredException('Votre compte n\'a pas été vérifier.');
         // }
+    }
+
+    public function resendVerifEmail(UserInterface $user)
+    {
+        return $this->emailVerifier->sendEmailConfirmation(
+            'app_verify_email',
+            $user,
+            (new TemplatedEmail())
+                ->from(new Address('gnut@gnut.eu', 'Gerald COL'))
+                ->to($user->getEmail())
+                ->subject('Please Confirm your Email')
+                ->htmlTemplate('registration/confirmation_email.html.twig')
+        );
     }
 }
